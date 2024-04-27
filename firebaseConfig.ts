2cf6105b4f8853f9317201726c6d74d0d17ee6fd
KameronW <52764265+KameronWelch@users.noreply.github.com>
2024-04-26 22:52:29 -0400

    Deleted Api info

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -10,13 +10,7 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBeYdJG2GI0uo10747sxjawIeVSrg_Hu_0",
-  authDomain: "budgetapp-4ff22.firebaseapp.com",
-  projectId: "budgetapp-4ff22",
-  storageBucket: "budgetapp-4ff22.appspot.com",
-  messagingSenderId: "816149159641",
-  appId: "1:816149159641:web:a31a8f3735d883da9368c8",
-  measurementId: "G-HTGG5LKDKT"
+
 };
 
 // Initialize Firebase
